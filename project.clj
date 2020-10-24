4441c86b7a5d884a1388ea4c044e54980c452101
Baptiste Fontaine <b@ptistefontaine.fr>
2020-10-24 13:43:58 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bfontaine/grape "0.4.0"
+(defproject bfontaine/grape "0.4.1-SNAPSHOT"
   :description "Syntax-aware Grep-like for Clojure code"
   :url "https://github.com/bfontaine/grape"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
